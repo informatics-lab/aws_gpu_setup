d5142a4427aae2bd5ba1941fe2b61fb6f105e03d
Alex Hilson <alexhilsonwork@googlemail.com>
2018-01-30 11:14:13 +0000

    Update to newer ami

--- modules/aws_dl/main.tf
+++ modules/aws_dl/main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "aws_dl" {
   most_recent = true
   filter {
     name = "name"
-    values = ["Deep Learning AMI Ubuntu Linux*"]
+    values = ["Deep Learning AMI (Ubuntu) Version 3.*"]
   }
 
   owners = ["898082745236"]
